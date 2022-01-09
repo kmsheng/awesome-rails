d0baa6f223c64927eb2db3726463a92d6d37bb09
kmsheng <kmsh3ng@gmail.com>
2022-01-09 16:52:56 +0800

    add unsigned int for articles creation

--- db/migrate/20211204084549_create_articles.rb
+++ db/migrate/20211204084549_create_articles.rb
@@ -1,6 +1,7 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
+      t.bigint :id, unsigned: true, null: false
       t.string :title
       t.text :body
 
