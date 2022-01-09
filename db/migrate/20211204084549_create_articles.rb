4c0565a64e6478122e71b98d2cb7d91c1212bf2c
kmsheng <kmsh3ng@gmail.com>
2022-01-09 16:56:37 +0800

    fix create_table migration

--- db/migrate/20211204084549_create_articles.rb
+++ db/migrate/20211204084549_create_articles.rb
@@ -1,6 +1,7 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
-    create_table :articles do |t|
+    create_table :articles, id: false do |t|
+      t.bigint :id, unsigned: true, null: false
       t.string :title
       t.text :body
 
