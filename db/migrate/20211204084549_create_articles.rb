4396d48f4ee5a112d9bd92bc8d52f650e9d5a363
kmsheng <kmsh3ng@gmail.com>
2022-01-09 16:55:49 +0800

    Revert "add unsigned int for articles creation"
    This reverts commit d0baa6f223c64927eb2db3726463a92d6d37bb09.

--- db/migrate/20211204084549_create_articles.rb
+++ db/migrate/20211204084549_create_articles.rb
@@ -1,7 +1,6 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-      t.bigint :id, unsigned: true, null: false
       t.string :title
       t.text :body
 
