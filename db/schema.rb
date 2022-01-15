0cbd7d0c1bfe41dd52ea43b3d7b4df9e83814ae9
kmsheng <kmsh3ng@gmail.com>
2022-01-15 15:45:56 +0800

    update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_09_091631) do
+ActiveRecord::Schema.define(version: 2022_01_09_092037) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
