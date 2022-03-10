2c890ed4636545c1ef3f26086e2403ec4f898c75
kmsheng <kmsh3ng@gmail.com>
2022-03-10 14:47:43 +0800

    add custom validation message example to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ApplicationRecord
   include Visible
   has_many :comments, dependent: :destroy
 
-  validates :title, presence: true
+  validates :title, presence: { message: 'title is needed' }
   validates :body, presence: true, length: { minimum: 5 }
 end
